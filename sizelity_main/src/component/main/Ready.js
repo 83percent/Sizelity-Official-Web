5eba678a0d74bbcc63f731387c8e9c392425ca61
JaeHoon Lee <hoonni2709@naver.com>
2021-07-16 15:26:56 +0900

    준비사항에 FAQ

--- sizelity_main/src/component/main/Ready.js
+++ sizelity_main/src/component/main/Ready.js
@@ -27,6 +27,15 @@ const Ready = () => {
                     return null;
                 }
             }
+        },
+        faqToggle : function(target) {
+            for(let i=0; i<5; ++i) {
+                if(target.nodeName !== "LI") {
+                    target = target.parentElement;
+                } else break;
+            }
+            if(target.nodeName !== "LI") return;
+            target.classList.toggle("on");
         }
     }
 
@@ -120,11 +129,70 @@ const Ready = () => {
                         <i className="material-icons">link</i>
                     </div>
                     <h1>사이즈리티 링크 버튼.</h1>
-                    <h2></h2>
                 </div>
-                <div>
-                    
+                
+                <div className="link-url-frame">
+                    <h2>아래의 링크로 이동하는 버튼을 쇼핑몰에 만들어주세요.</h2>
+                    <p>생성된 버튼을 누르고 고객님들이 여러분의 상품 사이즈비교를 할거요.</p>
+                    <div className="link-url en">
+                        <p>https://www.sizelity.com/compare</p>
+                    </div>
                 </div>
+            </section>
+            <section className="faq">
+                <div className="title">
+                    <h1>궁금한게 있나요?</h1>
+                </div>
+                <ul>
+                    <li onClick={(e) => event.faqToggle(e.target)}>
+                        <div className="question">
+                            <h3>관리자 링크를 받지 못했어요.</h3>
+                            <i className="material-icons">keyboard_arrow_down</i>
+                        </div>
+                        <div className="answer">
+                            <p>사전등록 하셨나요? 현재 서비스 오픈 전 입니다. 8월 중 서비스 오픈 전, 사전등록 하신 이메일로 관리자 링크를 보내드립니다.</p>
+                        </div>
+                    </li>
+                    <li onClick={(e) => event.faqToggle(e.target)}>
+                        <div className="question">
+                            <h3>링크 버튼은 왜 만들어야 하는거에요?</h3>
+                            <i className="material-icons">keyboard_arrow_down</i>
+                        </div>
+                        <div className="answer">
+                            <p>저희 사이즈리티는 설치 없는 서비스입니다. 업주님의 쇼핑몰에서 쇼핑하는 고객님들이 쇼핑 중 터치 또는 클릭 한번으로 손쉽게 지금 보는 상품의 사이즈 비교를 지원하기 위해서 필요합니다.</p>
+                        </div>
+                    </li>
+                    <li onClick={(e) => event.faqToggle(e.target)}>
+                        <div className="question">
+                            <h3>꼭 위 사진 처럼 링크 버튼을 만들어야 하나요?</h3>
+                            <i className="material-icons">keyboard_arrow_down</i>
+                        </div>
+                        <div className="answer">
+                            <p>아닙니다. 사이즈리티의 사이즈비교 서비스라는 것을 들어낼 수 있는 아이콘을 포함한 어떤 디자인 및 배치를 자유롭게 쇼핑몰에 맞게 하셔도 무관합니다.</p>
+                        </div>
+                    </li>
+                    <li onClick={(e) => event.faqToggle(e.target)}>
+                        <div className="question">
+                            <h3>링크 버튼을 만들 줄 몰라요.</h3>
+                            <i className="material-icons">keyboard_arrow_down</i>
+                        </div>
+                        <div className="answer">
+                            <p>죄송합니다. 너무 다양한 쇼핑몰 운영솔루션으로 인해, 구체적인 방법을 안내해드리기 어렵습니다.</p>
+                            <p>하지만, 사용하시는 운영 솔루션 업체에 "쇼핑몰 오른쪽 하단에 고정되는 링크 버튼" 과 관련된 문의를 하시면됩니다.</p>
+                            <p>(스마트 스토어를 제외한 모든 운영솔루션에서 해당 기능 지원 중)</p>
+                        </div>
+                    </li>
+                    <li onClick={(e) => event.faqToggle(e.target)}>
+                        <div className="question">
+                            <h3>'상품 상세보기에 링크버튼을 배치...'가 무슨말이에요?</h3>
+                            <i className="material-icons">keyboard_arrow_down</i>
+                        </div>
+                        <div className="answer">
+                            <p>사이즈리티 기능이 작동하려면 하나의 상품, 특정 상품 상세보기 화면에서 버튼을 눌러야지 기능을 지원합니다.</p>
+                            <p>그러므로 굳이 상세보기 화면을 제외한 다른 곳에서 링크 버튼이 노출될 필요는 없습니다.</p>
+                        </div>
+                    </li>
+                </ul>
             </section>
         </main>
     )
