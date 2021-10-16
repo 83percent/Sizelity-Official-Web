55a81b17ae7535aba1a684850985f26945150d15
JaeHoon Lee <hoonni2709@naver.com>
2021-10-16 17:53:36 +0900

    Backend Destory

--- backend/lib/models/ProductModel.js
+++
@@ -1,6 +0,0 @@
-const Mongoose = require('mongoose');
-const COLL_NAME = 'product';
-
-const ProductModel = new Mongoose.Schema({},{ collection : 'products'});
-
-module.exports = Mongoose.model(COLL_NAME, ProductModel);
--- backend/lib/models/ShopModel.js
+++
@@ -1,6 +0,0 @@
-const Mongoose = require('mongoose');
-const COLL_NAME = 'shop';
-
-const UserModel = new Mongoose.Schema({},{ collection : 'shops'});
-
-module.exports = Mongoose.model(COLL_NAME, UserModel);
